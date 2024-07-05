e274b137c9222ff88fc3b889f1edbc9e83c00331
Lovely-logesh <146011778+Lovely-logesh@users.noreply.github.com>
2024-07-05 13:22:19 -0400

    Update app.py
    using streamlit file uploader instead of google collab files

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
 #import sklearn.datasets
-from google.colab import files
+#from google.colab import files
 from sklearn.model_selection import train_test_split
 
 # loading the data from sklearn
