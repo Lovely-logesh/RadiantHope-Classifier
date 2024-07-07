e36c0854dd124415b1a0139c487a5dc22f02cd49
Lovely-logesh <146011778+Lovely-logesh@users.noreply.github.com>
2024-07-07 03:55:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
 import sklearn.datasets
+from sklearn.datasets import load_breast_cancer
 #from google.colab import files
 from sklearn.model_selection import train_test_split
 
